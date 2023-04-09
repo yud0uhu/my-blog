bd65487a2c898e8d88b89a1e2572e8dafaec3d36
denham <denham@denhamMacBook-Air.local>
2023-04-10 00:17:30 +0900

    style: 記事のfeed一覧を取得し表示するまで

---
+++ pages/index.tsx
@@ -0,0 +1,62 @@
+import Layout from "../components/layout";
+import gql from "graphql-tag";
+import client from "../lib/apollo-client";
+import Post, { PostProps } from "../components/post";
+
+const Blog: React.FC<{ data: { feed: PostProps[] } }> = (props) => {
+  return (
+    <Layout>
+      <div className="page">
+        <h1>My Blog</h1>
+        <main>
+          {props.data.feed.map((post) => (
+            <div key={post.id} className="post">
+              <Post post={post} />
+            </div>
+          ))}
+        </main>
+      </div>
+      <style jsx>{`
+        .post {
+          background: white;
+          transition: box-shadow 0.1s ease-in;
+        }
+
+        .post:hover {
+          box-shadow: 1px 1px 3px #aaa;
+        }
+
+        .post + .post {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query: gql`
+      query FeedQuery {
+        feed {
+          id
+          title
+          content
+          published
+          author {
+            id
+            name
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default Blog;
