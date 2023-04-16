f70d29cf1242663b00bf089ef58f1fbc74b448f3
denham <denham@denhamMacBook-Air.local>
2023-04-16 14:27:47 +0900

    feat: クライアント側でwasmを動かすための環境設定

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    config.output.webassemblyModuleFilename =
+      (isServer ? "../" : "") + "static/wasm/[modulehash].wasm";
+    return config;
+  },
+};
