d9c4fd2cd852feef731e4ac52b60570767a352b1
denham <denham@denhamMacBook-Air.local>
2023-05-06 12:52:50 +0900

    fix: ビルドプロセス中にprisma generateが走るように修正

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,4 +1,5 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+npx prisma generate
 npm run build:wasm && npm run build:next
