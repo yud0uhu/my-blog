2f035a972f0247cc7d5d77405bdc0b90fc3b437b
denham <denham@denhamMacBook-Air.local>
2023-07-01 02:52:14 +0900

    fix: vercelデプロイのためsecretを記述する

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
+  secret: process.env.NEXT_PUBLIC_SECRET,
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
