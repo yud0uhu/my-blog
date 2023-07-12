29eda8f1908b1257e4501316ea7c8d6edb004502
denham <denham@denhamMacBook-Air.local>
2023-07-12 22:51:54 +0900

    fix: 非ログイン時は404ページにリダイレクトするように修正

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -5,6 +5,7 @@ import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
 import init, { text_to_token } from "../../markdown-parser/pkg/markdown_parser";
 import Create from "../../features/create/components/Create";
+import { useSession } from "next-auth/react";
 
 // const CreateDraftMutation = gql`
 //   mutation CreateDraftMutation($title: String!, $content: String) {
@@ -17,13 +18,17 @@ import Create from "../../features/create/components/Create";
 // `;
 
 function CreatePage() {
+  const { data: session } = useSession();
   // init関数は、コンポーネントのマウント時ではなく、外部のebAssemblyモジュールを非同期でロードするため、useEffectフックを使用する
   useEffect(() => {
+    if (!session) {
+      Router.push("/404");
+    }
     const loadWasm = async () => {
       await init();
     };
     loadWasm();
-  }, []);
+  }, [session]);
 
   return (
     <Layout>
