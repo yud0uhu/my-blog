be895549bd119689798bca52abd361fda18c06cc
denham <denham@denhamMacBook-Air.local>
2023-07-12 20:52:22 +0900

    fix: ダークモードの状態をローカルストレージに保持するように

--- components/elements/header/Header.tsx
+++ components/elements/header/Header.tsx
@@ -3,13 +3,22 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Router from "next/router";
 import { useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
-import { ButtonContainer, Button } from "../../layout";
+import {
+  ButtonContainer,
+  StyledButton,
+  MenuIcon,
+  MenuItem,
+  MenuContainer,
+} from "../../layout";
 import { GlobalStyle } from "./styles/HeaderStyles";
 
 const Header: React.FC = () => {
   const { data: session } = useSession();
   const [colorScheme, setColorScheme] = useState("light");
   useEffect(() => {
+    const localStorageTheme = localStorage.getItem("theme");
+    setColorScheme(localStorageTheme || "light");
+
     document.documentElement.style.setProperty(
       "--header-background-color",
       colorScheme === "light" ? "white" : "dark"
@@ -27,13 +36,14 @@ const Header: React.FC = () => {
       colorScheme === "light" ? "black" : "white"
     );
     document.documentElement.style.setProperty(
-      "--textarea-background-color",
+      "--textarea-background-color:",
       colorScheme === "light" ? "white" : "#0E1117"
     );
   }, [colorScheme]);
   const handleColorSchemeChange = () => {
     const newColorScheme = colorScheme === "light" ? "dark" : "light";
     setColorScheme(newColorScheme);
+    localStorage.setItem("theme", newColorScheme);
     document.documentElement.style.setProperty(
       "--background-color",
       newColorScheme === "light" ? "#f6f8fa" : "#02040A"
@@ -47,41 +57,47 @@ const Header: React.FC = () => {
   const handleSignIn = async () => {
     await signIn();
   };
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
       <GlobalStyle />
       <div className="header">
-        <ActionIcon
-          className="icon"
-          variant="outline"
-          color={colorScheme === "dark" ? "yellow" : "blue"}
-          onClick={() => handleColorSchemeChange()}
-          title="Toggle color scheme"
-        >
-          {colorScheme === "dark" ? (
-            <FaSun size="1.1rem" />
-          ) : (
-            <FaMoon size="1.1rem" />
-          )}
-        </ActionIcon>
-
         <ButtonContainer>
+          <ActionIcon
+            className="icon"
+            variant="outline"
+            color={colorScheme === "dark" ? "yellow" : "blue"}
+            onClick={() => handleColorSchemeChange()}
+            title="Toggle color scheme"
+          >
+            {colorScheme === "dark" ? (
+              <FaSun size="1.1rem" />
+            ) : (
+              <FaMoon size="1.1rem" />
+            )}
+          </ActionIcon>
+
           {session ? (
             <>
-              <Button className="button" onClick={handleSignOut}>
-                ログアウト
-              </Button>
-              <Button className="button" onClick={() => Router.push("/create")}>
-                投稿する
-              </Button>
+              <MenuIcon
+                src="https://github.com/yud0uhu.png"
+                alt="Menu"
+                onClick={handleMenuToggle}
+              />
+              <MenuContainer isOpen={isOpen}>
+                <MenuItem onClick={handleSignOut}>ログアウト</MenuItem>
+                <MenuItem onClick={() => Router.push("/create")}>
+                  投稿する
+                </MenuItem>
+              </MenuContainer>
             </>
           ) : (
-            <>
-              <Button className="button" onClick={handleSignIn}>
-                ログイン
-              </Button>
-            </>
+            <StyledButton onClick={handleSignIn}>ログイン</StyledButton>
           )}
         </ButtonContainer>
       </div>
