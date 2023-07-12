a083766537c84e549f6371e08e53fa5a21d6bb13
denham <denham@denhamMacBook-Air.local>
2023-07-13 02:30:18 +0900

    fix: sessionフラグが検証用のままになっていたため修正

--- components/elements/header/Header.tsx
+++ components/elements/header/Header.tsx
@@ -82,7 +82,7 @@ const Header: React.FC = () => {
             )}
           </ActionIcon>
 
-          {!session ? (
+          {session ? (
             <>
               <MenuIcon
                 src="https://github.com/yud0uhu.png"
