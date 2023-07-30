195f24d60abd6c8bdd99b5d8fe3ff0dfa5e619e5
denham <denham@denhamMacBook-Air.local>
2023-07-30 23:43:20 +0900

    fix: 検証のためアクセストークンをすべて直書き

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,12 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, NEXT_PUBLIC_SECRET } =
-  process.env;
+// const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, NEXT_PUBLIC_SECRET } =
+//   process.env;
+
+const GITHUB_CLIENT_ID = "4a77e0d2424aa3e623e0";
+const GITHUB_CLIENT_SECRET = "07eb52efcd3e87f5dfa47d5d8e5e61f4158e8c4a";
+const NEXT_PUBLIC_SECRET = "4bRXpaP8Vi01ldHky/F1XW7QyXkYtBKcAil2tVBaZg0=";
 
 if (!GITHUB_CLIENT_ID) throw new Error("You must provide GITHUB_ID env var.");
 if (!GITHUB_CLIENT_SECRET)
@@ -11,7 +15,7 @@ if (!NEXT_PUBLIC_SECRET)
   throw new Error("You must provide NEXT_PUBLIC_SECRET env var.");
 
 export const authOptions: NextAuthOptions = {
-  secret: "4bRXpaP8Vi01ldHky/F1XW7QyXkYtBKcAil2tVBaZg0=",
+  secret: NEXT_PUBLIC_SECRET,
   providers: [
     GithubProvider({
       clientId: GITHUB_CLIENT_ID || "",
