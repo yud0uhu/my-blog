f9ce0d4ae317f30a43a7cf593ec54c14c14de6f4
denham <denham@denhamMacBook-Air.local>
2023-07-30 19:50:57 +0900

    fix: 各トークンをを環境変数から取得するように

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,12 +2,12 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
-  secret: "4bRXpaP8Vi01ldHky/F1XW7QyXkYtBKcAil2tVBaZg0=",
+  secret: process.env.SECRET,
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: "4a77e0d2424aa3e623e0",
-      clientSecret: "c15befdd5cfdda48a9cf24f82ce70876ed9622ac",
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
     }),
     // ...add more providers here
   ],
