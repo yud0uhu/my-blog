eb7dec743db7b6a2abc8e929b4ad1d6e8edb777f
denham <denham@denhamMacBook-Air.local>
2023-08-09 07:01:14 +0900

    Revert "fix: Vercelでは環境変数不要のため"
    This reverts commit db53c8df3ce31d75819147f9a9ff755acbf4478e.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,16 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-if (!process.env.GITHUB_ID)
-  throw new Error("You must provide GITHUB_ID env var.");
-if (!process.env.GITHUB_SECRET)
-  throw new Error("You must provide GITHUB_SECRET env var.");
+// console.log(process.env.GITHUB_ID);
+// if (!process.env.GITHUB_ID)
+//   throw new Error("You must provide GITHUB_ID env var.");
+// if (!process.env.GITHUB_SECRET)
+//   throw new Error("You must provide GITHUB_SECRET env var.");
+// if (!process.env.NEXT_PUBLIC_SECRET)
+//   throw new Error("You must provide NEXT_PUBLIC_SECRET env var.");
 
-/**
- * Docs: https://next-auth.js.org/deployment
- */
 export const authOptions: NextAuthOptions = {
-  // secret: process.env.NEXT_PUBLIC_SECRET || "",
+  secret: process.env.NEXT_PUBLIC_SECRET || "",
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
