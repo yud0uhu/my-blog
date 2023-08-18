f6d490bbf25941077ee99e8f25dd5b37c5593413
denham <denham@denhamMacBook-Air.local>
2023-08-18 16:36:54 +0900

    fix: タグが配列に追加されない不具合を修正

--- features/create/components/tag-input/TagInput.tsx
+++ features/create/components/tag-input/TagInput.tsx
@@ -1,16 +1,16 @@
-import { Badge } from '@mantine/core'
 import React, { useState } from 'react'
+import { Badge } from '@mantine/core'
 import { FaTags, FaTimes } from 'react-icons/fa'
 import { StyledTextInput } from '../../../../components/layout/styles'
 
 interface TagInputProps {
-  tags: string[]
-  onTagAdd: () => void
+  onTagAdd: (tag: string) => void
   onTagRemove: (tag: string) => void
 }
 
-const TagInput: React.FC<TagInputProps> = ({ tags, onTagAdd, onTagRemove }) => {
+const TagInput: React.FC<TagInputProps> = ({ onTagAdd, onTagRemove }) => {
   const [tagInput, setTagInput] = useState('')
+  const [tags, setTags] = useState<string[]>([])
 
   const handleTagInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTagInput(e.target.value)
@@ -19,8 +19,12 @@ const TagInput: React.FC<TagInputProps> = ({ tags, onTagAdd, onTagRemove }) => {
   const handleEnterKey = (e: React.KeyboardEvent) => {
     if (e.key === 'Enter') {
       e.preventDefault()
-      if (tagInput.trim() !== '') {
-        onTagAdd()
+      const trimmedTagInput = tagInput.trim()
+      if (trimmedTagInput !== '') {
+        onTagAdd(trimmedTagInput)
+        setTags([...tags, trimmedTagInput]) // タグを追加したらtagsステートも更新する
+        console.log(trimmedTagInput)
+        setTagInput('')
       }
     }
   }
